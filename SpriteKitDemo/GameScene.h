2ad3772c28293bed4122165523113d2e59d2b36a
opendriod <ajaythakur@hotmail.com>
2016-04-08 11:21:17 -0700

    Initial Commit

---
+++ SpriteKitDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SpriteKitDemo
+//
+
+//  Copyright (c) 2016 Ajay Thaur. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SpriteKitDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SpriteKitDemo
+//
+
+//  Copyright (c) 2016 Ajay Thaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
